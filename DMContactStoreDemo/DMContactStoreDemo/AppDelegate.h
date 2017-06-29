35fd8af69c3a680c8e8f2000d8aff9dc9f4bc39b
lichan <10915819@qq.com>
2017-06-29 11:39:40 +0800

    [add] first commit

---
+++ DMContactStoreDemo/DMContactStoreDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DMContactStoreDemo
+//
+//  Created by 李二狗 on 2017/6/29.
+//  Copyright © 2017年 YRHY Science and Technology (Beijing) Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DMContactStoreDemo/DMContactStoreDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DMContactStoreDemo
+//
+//  Created by 李二狗 on 2017/6/29.
+//  Copyright © 2017年 YRHY Science and Technology (Beijing) Co., Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
